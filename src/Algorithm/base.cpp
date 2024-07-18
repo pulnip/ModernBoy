1a5b559389641ef8d98ad86ed173343c9ca84646
pulnip <choiwh1008@gmail.com>
2024-07-18 23:22:21 +0900

    Update base.cpp

--- src/Algorithm/base.cpp
+++ src/Algorithm/base.cpp
@@ -5,45 +5,47 @@ using namespace std;
 using namespace gsl;
 using namespace ModernBoy;
 
-class Sieve{
-    vector<bool> odds;
+namespace ModernBoy{
+    class Sieve{
+        vector<bool> odds;
 
-    void mark(index pn, index p) noexcept{
-        auto end = odds.size();
-        assert(pn < end);
+        void mark(index pn, index p) noexcept{
+            auto end = odds.size();
+            assert(pn < end);
 
-        for(auto i=half(pn-3); i<end; i+=p){
-            odds[i]=false;
-        }
-    }
-
-    void sift(index unchecked) noexcept{
-        auto m_1 = pow2(odds.size(), 1) + 3;
-
-        for(index p=3; p*p < m_1; p+=2){
-            auto pp = p*p;
-            auto pn = unchecked - unchecked%p;
-            auto first = std::max(pn, pp);
-        
-            mark(first, p);
-        }
-    }
-
-  public:
-    bool isPrime(index n){
-        assert(2<n && odd(n));
-
-        auto unchecked=pow2(odds.size(), 1)+3;
-        auto idx = half(n-3);
-
-        if(unchecked <= n){
-            odds.resize(idx + 1, true);
-            sift(unchecked);
+            for(auto i=half(pn-3); i<end; i+=p){
+                odds[i]=false;
+            }
         }
 
-        return odds[idx];
-    }
-};
+        void sift(index unchecked) noexcept{
+            auto m_1 = pow2(odds.size(), 1) + 3;
+
+            for(index p=3; p*p < m_1; p+=2){
+                auto pp = p*p;
+                auto pn = unchecked - unchecked%p;
+                auto first = std::max(pn, pp);
+            
+                mark(first, p);
+            }
+        }
+
+      public:
+        bool isPrime(index n){
+            assert(2<n && odd(n));
+
+            auto unchecked=pow2(odds.size(), 1)+3;
+            auto idx = half(n-3);
+
+            if(unchecked <= n){
+                odds.resize(idx + 1, true);
+                sift(unchecked);
+            }
+
+            return odds[idx];
+        }
+    };
+}
 
 bool ModernBoy::isPrime(index n) noexcept{
     static Sieve sieve;
