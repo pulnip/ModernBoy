9a114c36d68f0dbbd677b5e37464b6789f84848b
pulnip <choiwh1008@gmail.com>
2024-07-18 21:25:41 +0900

    add wrap, clamp test

--- test/Math_test.cpp
+++ test/Math_test.cpp
@@ -4,6 +4,9 @@
 using namespace glm;
 using namespace ModernBoy;
 
+constexpr Line2 WINDOW_RANGE{uvTopLeft, uvBottomRight};
+constexpr Line fCOLOR_RANGE{fBLACK, fWHITE};
+
 TEST(MathTest, lerpf_test){
     EXPECT_FLOAT_EQ(lerp({0.0f, 10.0f}, 0.5f), 5.0f);
     EXPECT_FLOAT_EQ(lerp({0.0f, 5.0f}, 0.2f), 1.0f);
@@ -21,10 +24,17 @@ TEST(MathTest, lerpv_test){
 }
 
 TEST(MathTest, in_test){
-    EXPECT_EQ(in(3.4f, {1.0f, 10.0f}), true);
-    EXPECT_EQ(in(12.4f, {1.0f, 10.0f}), false);
-    EXPECT_EQ(in(fDUNE, {fBLACK, fWHITE}), true);
-    EXPECT_EQ(in(fWHITE, {fBLACK, fDUNE}), false);
+    EXPECT_TRUE(in(3.4f, {1.0f, 10.0f}));
+    EXPECT_FALSE(in(12.4f, {1.0f, 10.0f}));
+
+    EXPECT_TRUE(in({0.3f, 0.7f}, WINDOW_RANGE));
+    EXPECT_FALSE(in({0.3f, 1.1f}, WINDOW_RANGE));
+    EXPECT_FALSE(in({-0.1f, 0.7f}, WINDOW_RANGE));
+    EXPECT_FALSE(in({-0.1f, -0.1f}, WINDOW_RANGE));
+    EXPECT_FALSE(in({1.1f, 1.1f}, WINDOW_RANGE));
+
+    EXPECT_TRUE(in(fDUNE, fCOLOR_RANGE));
+    EXPECT_FALSE(in(fWHITE, {fBLACK, fDUNE}));
 }
 
 TEST(MathTest, wrap_test){
@@ -41,4 +51,43 @@ TEST(MathTest, clamp_test){
     EXPECT_FLOAT_EQ(clamp(6.3f, {1.0f, 5.0f}), 5.0f);
     EXPECT_FLOAT_EQ(clamp(6.3f, {12.4f, 20.3f}), 12.4f);
     EXPECT_FLOAT_EQ(clamp(34.5f, {12.3f, 45.6f}), 34.5f);
+}
+
+TEST(MathTest, wrap2_test){
+    constexpr vec2 origin1{0.7f, 1.3f};
+    constexpr vec2 origin2{1.1f, 0.7f};
+    constexpr vec2 origin3{1.2f, 1.2f};
+    auto wrapped1=wrap(origin1, WINDOW_RANGE);
+    auto wrapped2=wrap(origin2, WINDOW_RANGE);
+    auto wrapped3=wrap(origin3, WINDOW_RANGE);
+
+    EXPECT_TRUE(in(wrapped1, WINDOW_RANGE));
+    EXPECT_TRUE(in(wrapped2, WINDOW_RANGE));
+    EXPECT_TRUE(in(wrapped3, WINDOW_RANGE));
+
+    EXPECT_FLOAT_EQ(origin1.x, wrapped1.x);
+    EXPECT_FLOAT_EQ(wrapped1.y, 0.3f);
+    EXPECT_FLOAT_EQ(wrapped2.x, 0.1f);
+    EXPECT_FLOAT_EQ(origin2.y, wrapped2.y);
+    EXPECT_FLOAT_EQ(wrapped3.x, 0.2f);
+    EXPECT_FLOAT_EQ(wrapped3.y, 0.2f);
+}
+
+TEST(MathTest, clamp2_test){
+    constexpr vec2 origin1{0.7f, 1.3f};
+    constexpr vec2 origin2{1.1f, 0.7f};
+    constexpr vec2 origin3{1.2f, 1.2f};
+    auto clamped1=clamp(origin1, WINDOW_RANGE);
+    auto clamped2=clamp(origin2, WINDOW_RANGE);
+    auto clamped3=clamp(origin3, WINDOW_RANGE);
+
+    EXPECT_FALSE(in(clamped1, WINDOW_RANGE));
+    EXPECT_FALSE(in(clamped2, WINDOW_RANGE));
+    EXPECT_FALSE(in(clamped3, WINDOW_RANGE));
+
+    EXPECT_FLOAT_EQ(origin1.x, clamped1.x);
+    EXPECT_FLOAT_EQ(clamped1.y, 1.0f);
+    EXPECT_FLOAT_EQ(clamped2.x, 1.0f);
+    EXPECT_FLOAT_EQ(origin2.y, clamped2.y);
+    EXPECT_EQ(clamped3, uvBottomRight);
 }
