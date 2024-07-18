fb8e6f207c1b6557d541c63b414f0fb4a7b22636
pulnip <choiwh1008@gmail.com>
2024-07-18 13:59:34 +0900

    move sieve impl to .cpp

--- include/Algorithm/base.hpp
+++ include/Algorithm/base.hpp
@@ -3,7 +3,7 @@
 #include <cassert>
 #include <concepts>
 #include <iterator>
-#include <vector>
+#include <gsl-lite/gsl-lite.hpp>
 
 namespace ModernBoy{
     namespace Algorithm{
@@ -39,60 +39,6 @@ namespace ModernBoy{
             return multiply_aux(a, half(n-1), a+a);
         }
 
-        class Sieve{
-            inline static std::vector<bool> odds;
-
-            static void mark(Int auto pn, Int auto p) noexcept{
-                auto end = odds.size();
-                assert(pn < end);
-
-                for(auto i=half(pn-3); i<end; i+=p){
-                    odds[i]=false;
-                }
-            }
-
-            template<Int N> static void sift(N unchecked) noexcept{
-                auto m_1 = pow2(odds.size(), 1) + 3;
-
-                for(N p=3; p*p < m_1; p+=2){
-                    auto pp = p*p;
-                    auto pn = unchecked - unchecked%p;
-                    auto first = std::max(pn, pp);
-                
-                    mark(first, p);
-                }
-            }
-
-          public:
-            static bool isPrime(Int auto n){
-                assert(2<n && odd(n));
-
-                auto unchecked=pow2(odds.size(), 1)+3;
-                auto idx = half(n-3);
-
-                if(unchecked <= n){
-                    odds.resize(idx + 1, true);
-                    sift(unchecked);
-                }
-
-                return odds[idx];
-            }
-        };
-
-        bool isPrime(Int auto n) noexcept{
-            switch(n){
-            case 0:
-                [[fallthrough]];
-            case 1:
-                return false;
-            case 2:
-                return true;
-            default:
-                if(odd(n))
-                    return Sieve::isPrime(n);
-                else
-                    return false;
-            }
-        }
+        bool isPrime(gsl::index n) noexcept;
     }
 }
--- src/Algorithm/base.cpp
+++ src/Algorithm/base.cpp
@@ -1,3 +1,65 @@
+#include <vector>
 #include "Algorithm/base.hpp"
 
+using namespace std;
+using namespace gsl;
+using namespace ModernBoy;
 using namespace ModernBoy::Algorithm;
+
+class Sieve{
+    vector<bool> odds;
+
+    void mark(index pn, index p) noexcept{
+        auto end = odds.size();
+        assert(pn < end);
+
+        for(auto i=half(pn-3); i<end; i+=p){
+            odds[i]=false;
+        }
+    }
+
+    void sift(index unchecked) noexcept{
+        auto m_1 = pow2(odds.size(), 1) + 3;
+
+        for(index p=3; p*p < m_1; p+=2){
+            auto pp = p*p;
+            auto pn = unchecked - unchecked%p;
+            auto first = std::max(pn, pp);
+        
+            mark(first, p);
+        }
+    }
+
+  public:
+    bool isPrime(index n){
+        assert(2<n && odd(n));
+
+        auto unchecked=pow2(odds.size(), 1)+3;
+        auto idx = half(n-3);
+
+        if(unchecked <= n){
+            odds.resize(idx + 1, true);
+            sift(unchecked);
+        }
+
+        return odds[idx];
+    }
+};
+
+bool Algorithm::isPrime(index n) noexcept{
+    static Sieve sieve;
+
+    switch(n){
+    case 0:
+        [[fallthrough]];
+    case 1:
+        return false;
+    case 2:
+        return true;
+    default:
+        if(odd(n))
+            return sieve.isPrime(n);
+        else
+            return false;
+    }
+}
