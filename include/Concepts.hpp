129f87f869533e41a7a4b697aa6db4259ae9627d
pulnip <choiwh1008@gmail.com>
2024-08-06 01:00:10 +0900

    :sparkles: update Concepts.hpp

--- include/Concepts.hpp
+++ include/Concepts.hpp
@@ -6,22 +6,34 @@
 namespace std{
     template<typename S>
     concept scalar=std::is_scalar_v<S>;
+    template<typename P>
+    concept pointer=std::is_pointer_v<P>;
 }
 
 namespace ModernBoy{
     template<typename T>
-    concept multiplicative=requires(T lhs, T rhs){
+    concept multiplicative_monoid=requires(T lhs, T rhs){
         requires std::same_as<decltype(lhs*rhs), T>;
         requires std::same_as<std::remove_reference_t<decltype(lhs*=rhs)>, T>;
+    };
+    template<typename T>
+    concept multiplicative=multiplicative_monoid<T> && requires(T lhs, T rhs){
+        requires std::same_as<decltype(lhs/rhs), T>;
+        requires std::same_as<std::remove_reference_t<decltype(lhs/=rhs)>, T>;
     };
     template<typename L, typename R>
     concept multiply_commutative=requires(L lhs, R rhs){
         requires std::same_as<decltype(lhs*rhs), decltype(rhs*lhs)>;
     };
     template<typename T>
-    concept additive=requires(T lhs, T rhs){
+    concept additive_monoid=requires(T lhs, T rhs){
         requires std::same_as<decltype(lhs+rhs), T>;
         requires std::same_as<std::remove_reference_t<decltype(lhs+=rhs)>, T>;
+    };
+    template<typename T>
+    concept additive=additive_monoid<T> && requires(T lhs, T rhs){
+        requires std::same_as<decltype(lhs-rhs), T>;
+        requires std::same_as<std::remove_reference_t<decltype(lhs-=rhs)>, T>;
     };
     template<typename L, typename R>
     concept add_commutative=requires(L lhs, R rhs){
@@ -29,10 +41,10 @@ namespace ModernBoy{
     };
     template<typename T>
     concept scalar_multiplicative=std::scalar<T> || 
-    requires(typename T::value_type s, T t){
-        requires std::scalar<decltype(s)>;
-        requires multiply_commutative<T, decltype(s)>;
-        requires std::same_as<decltype(s*t), T>;
+    requires(T t){
+        requires std::scalar<decltype(t.x)>;
+        requires multiply_commutative<T, decltype(t.x)>;
+        requires std::same_as<decltype(t.x*t), T>;
     };
 
     template<typename T>
@@ -50,4 +62,11 @@ namespace ModernBoy{
     };
     template<typename V, typename P>
     concept affine_additive=linear<V> && point<P, V>;
+
+    template<typename T>
+    concept Dim4=requires(T t){ t.x; t.y; t.z; t.w; };
+    template<typename T>
+    concept Dim3 = !Dim4<T> && requires(T t){ t.x; t.y; t.z; };
+    template<typename T>
+    concept Dim2 = !Dim4<T> && !Dim3<T> && requires(T t){ t.x; t.y; };
 }
