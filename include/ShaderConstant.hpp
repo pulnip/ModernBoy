dca51dd125389c5f1dec3a330ae138e17ed71d7c
pulnip <choiwh1008@gmail.com>
2024-08-12 17:25:30 +0900

    :art: Separate two classes into different files

---
+++ include/ShaderConstant.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Attributes.hpp"
+
+#define MAX_LIGHTS 3
+
+namespace ModernBoy{
+    struct VSConstants{
+        DirectX::SimpleMath::Matrix model{};
+        DirectX::SimpleMath::Matrix invTranspose{};
+        DirectX::SimpleMath::Matrix view{};
+        DirectX::SimpleMath::Matrix projection{};
+    };
+    // check 16-byte aligned
+    static_assert((sizeof(VSConstants) & 0xf) == 0);
+
+    struct PSConstants{
+        // float xSplit=0.0f;
+        // float dummy[3]={0.0f, 0.0f, 0.0f};
+        Vector3 eyePos;
+        bool useTexture;
+        Material material;
+        Light lights[MAX_LIGHTS];
+    };
+    // check 16-byte aligned
+    static_assert((sizeof(PSConstants) & 0xf) == 0);
+}
--- include/Vertex.hpp
+++ include/Vertex.hpp
@@ -3,25 +3,15 @@
 #include "Math.hpp"
 
 namespace ModernBoy{
-    struct Vertex{
+    struct ColorVertex{
         WorldPos position;
         Vector3 normal;
         Color color;
+    };
+
+    struct Vertex{
+        WorldPos position;
+        Vector3 normal;
         UVPos uv;
     };
-
-    struct VSConstants{
-        DirectX::SimpleMath::Matrix model{};
-        DirectX::SimpleMath::Matrix view{};
-        DirectX::SimpleMath::Matrix projection{};
-    };
-    // check 16-byte aligned
-    static_assert((sizeof(VSConstants) & 0xf) == 0);
-
-    struct PSConstants{
-        float xSplit=0.0f;
-        float dummy[3]={0.0f, 0.0f, 0.0f};
-    };
-    // check 16-byte aligned
-    static_assert((sizeof(PSConstants) & 0xf) == 0);
 }
