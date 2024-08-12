8d347184abd5950547e6297f91fd60171c2d1031
pulnip <choiwh1008@gmail.com>
2024-08-12 17:26:53 +0900

    :art: combine Light class into Attribute.hpp

--- include/Attributes.hpp
+++ include/Attributes.hpp
@@ -9,12 +9,10 @@
 
 namespace ModernBoy{
     struct Transform{
-        DirectX::SimpleMath::Vector3 position
-            =DirectX::SimpleMath::Vector3::Zero;
+        Vector3 position=Vector3::Zero;
         DirectX::SimpleMath::Quaternion quaternion
             =DirectX::SimpleMath::Quaternion::Identity;
-        DirectX::SimpleMath::Vector3 scale
-            =DirectX::SimpleMath::Vector3::One;
+        Vector3 scale=Vector3::One;
 
         DirectX::SimpleMath::Matrix transform() const;
 
@@ -23,14 +21,26 @@ namespace ModernBoy{
     };
 
     struct Material{
-        DirectX::SimpleMath::Color ambient{0.2f, 0.2f, 0.2f};
-        DirectX::SimpleMath::Color diffuse=DirectX::Colors::Blue.v;
-        DirectX::SimpleMath::Color specular=DirectX::Colors::White.v;
-        float shininess=10.0f;
-
+        Vector3 ambient{0.1f, 0.1f, 0.1f};
+        float shininess=1.0f;
+        Vector3 diffuse{0.5f, 0.5f, 0.5f};
         float reflection=0.0f;
+        Vector3 specular{0.5f, 0.5f, 0.5f};
         float transparency=0.0f;
     };
+    // check 16-byte aligned
+    static_assert((sizeof(Material) & 0xf) == 0);
+
+    struct Light{
+        Vector3 strength=Vector3::One;
+        float fallOffStart=0.0f;
+        Vector3 dir=Vector3::UnitZ;
+        float fallOffEnd=10.0f;
+        Vector3 pos=-2*Vector3::UnitZ;
+        float spotPower=1.0f;
+    };
+    // check 16-byte aligned
+    static_assert((sizeof(Light) & 0xf) == 0);
 
     struct Polygon{
         gsl::index index[3];
