bf944793afc2281928b2a02680f8b3245b145dc8
pulnip <choiwh1008@gmail.com>
2025-06-09 17:19:04 +0900

    use SwiftPM structure for Swift intellisense

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ModernBoy",
+    targets: [
+        .target(
+            name: "ModernBoy",
+            path: "src/backends/metal",
+            exclude: [
+                "context.cpp",
+                "mesh.cpp"
+            ]
+        )
+    ]
+)
