2026b00352c098a770f57d06331e4c35ebde1489
pulnip <choiwh1008@gmail.com>
2025-06-09 19:44:14 +0900

    remove submodule metal-cpp

--- include/backends/metal/type.hpp
+++ include/backends/metal/type.hpp
@@ -1,14 +1,9 @@
 #ifndef MODERNBOY_METAL_TYPE_HPP
 #define MODERNBOY_METAL_TYPE_HPP
 
-#include <memory>
-#include <Foundation/Foundation.hpp>
-#include <Metal/Metal.hpp>
-#include <QuartzCore/QuartzCore.hpp>
-
 namespace ModernBoy::Metal
 {
-    using BufferPtr = NS::SharedPtr<MTL::Buffer>;
+    using BufferPtr = void*;
 }
 
 #endif // MODERNBOY_METAL_TYPE_HPP
