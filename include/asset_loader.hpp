bfa8032bf6df31ad8d9225359837b9733604f501
pulnip <choiwh1008@gmail.com>
2025-06-14 22:25:49 +0900

    add support for loading transform from TOML in AssetLoader

--- include/asset_loader.hpp
+++ include/asset_loader.hpp
@@ -27,7 +27,19 @@ namespace ModernBoy{
             toml::table tbl = toml::parse_file(fileName);
 
             std::string name = *tbl["name"].value<std::string>();
-            auto transformHandle = transformManager.create(Transform{});
+
+            auto t=tbl["transform"].as_table();
+            Transform transform{};
+            auto p = (*t)["position"].as_array();
+            auto r = (*t)["rotation"].as_array();
+            auto s = (*t)["scale"].as_array();
+            for(size_t i=0; i<3; ++i)
+                transform.position[i] = *((*p)[i]).value<double>();
+            for(size_t i=0; i<4; ++i)
+                transform.rotation[i] = *((*r)[i]).value<double>();
+            for(size_t i=0; i<3; ++i)
+                transform.scale[i] = *((*s)[i]).value<double>();
+            auto transformHandle = transformManager.create(std::move(transform));
 
             std::string meshFile = *tbl["mesh"]["file"].value<std::string>();
             auto meshHandles = meshLoader.load(meshFile);
