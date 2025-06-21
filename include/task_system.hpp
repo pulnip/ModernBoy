dae28fa0122a4697727fde05eeed338b5464e9af
pulnip <choiwh1008@gmail.com>
2025-06-21 15:34:11 +0900

    rename TaskManager to TaskSystem

---
+++ include/task_system.hpp
@@ -0,0 +1,37 @@
+#ifndef MODERNBOY_TASK_SYSTEM_HPP
+#define MODERNBOY_TASK_SYSTEM_HPP
+
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include "resource_handle.hpp"
+
+namespace ModernBoy
+{
+    template<typename Component, typename Task>
+    class TaskSystem{
+    private:
+        std::vector<Component> components;
+
+    public:
+        template<typename... Args>
+        void create(Args... args){
+            components.emplace_back(args...);
+        }
+
+        auto getAll() const{
+            std::vector<Task> tasks;
+
+            std::for_each(components.cbegin(), components.cend(),
+                [&wrapped](const auto& component){
+                    auto newTasks = component.getTasks();
+                    tasks.append_range(newTasks);
+                }
+            );
+
+            return tasks;
+        }
+    };
+} // namespace ModernBoy
+
+#endif // MODERNBOY_TASK_SYSTEM_HPP
