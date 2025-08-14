1dff2fe683ba10b520e3ac6665cfbc5748a81c02
pulnip <choiwh1008@gmail.com>
2025-08-14 16:58:33 +0900

    define my custom mesh file format

---
+++ include/engine/asset/raw_resource.hpp
@@ -0,0 +1,16 @@
+#ifndef MODERNBOY_ASSET_RAWRESOURCE_HPP
+#define MODERNBOY_ASSET_RAWRESOURCE_HPP
+
+#include "core/math/type.hpp"
+
+namespace ModernBoy::Asset
+{
+    union Vertex{
+        Vec3 position;
+        Vec3 normal;
+        Vec2 texcoord;
+        Vec4 tangent;
+    };
+} // namespace ModernBoy::Asset
+
+#endif // MODERNBOY_ASSET_RAWRESOURCE_HPP
